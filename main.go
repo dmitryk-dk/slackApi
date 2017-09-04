73b6d22fb3596387ba9e85c471337046335a88ce
dmitryk-dk <kozlovdmitriyy@gmail.com>
2017-09-04 18:13:02 +0300

    remove unused struct

--- main.go
+++ main.go
@@ -8,26 +8,6 @@ import (
 	"log"
 )
 
-type Data struct {
-	Ok    bool   `json:"ok"`
-	URL   string `json:"url"`
-	Error string `json:"error"`
-}
-
-type Channels struct {
-	Ok    bool                `json:"ok"`
-	List  []map[string]string `json:"channels"`
-	Error string              `json:"error"`
-}
-
-type Message struct {
-	ID      string `json:"id"`
-	ReplyTo string `json:"reply_to"`
-	Type    string `json:"type"`
-	Channel string `json:"channel"`
-	Text    string `json:"text"`
-}
-
 func main() {
 	slackResponse, err := api.SlackConnect()
 	if err != nil {
